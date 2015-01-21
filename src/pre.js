b00d9cd1f6b81d122d6593cab145b0c3475c7528
DerKoun <e-male@web.de>
2015-01-21 08:53:54 +0100

    made it possible to override AR
    Made it possible to override AR. Can be set to any positive value or to 0 to disable forcing any AR. 'undefined' is mapped to '4/3'. (idea and code by @dreamlayers)

--- src/pre.js
+++ src/pre.js
@@ -1,4 +1,6 @@
 // Don't copy canvas image back into RAM in SDL_LockSurface()
 Module['screenIsReadOnly'] = true;
-// Ensure 4:3 aspect ratio
-Module['forcedAspectRatio'] = 4 / 3;
+// Ensure 4:3 aspect ratio, unless specified otherwise
+if (typeof(Module['forcedAspectRatio']) == 'undefined') {
+    Module['forcedAspectRatio'] = 4 / 3;
+}
